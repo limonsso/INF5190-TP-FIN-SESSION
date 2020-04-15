ea2a160d1ddfc9cb0a787ad2a0ac6ae6a0ba04b7
Ezechiel Gnepa <egnepa@unimax.ca>
2020-04-15 17:29:04 -0400

    active logging

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
+import logging
+import sys
+
 from webapp import app
 from webapi import api
 
 app.register_blueprint(api)
+handler = logging.StreamHandler(sys.stdout)
+app.logger.addHandler(handler)
 
 if __name__ == '__main__':
     app.run()
